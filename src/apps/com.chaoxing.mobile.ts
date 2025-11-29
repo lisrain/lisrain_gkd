899495e8548f679ce8171d2129b866ed60b6887e
Lisrain <89452996+lisrain@users.noreply.github.com>
2025-11-29 15:06:35 +0800

    Refactor app configuration for 学习通

--- src/apps/com.chaoxing.mobile.ts
+++ src/apps/com.chaoxing.mobile.ts
@@ -1,24 +1,24 @@
 import { defineGkdApp } from '@gkd-kit/define';
 
 export default defineGkdApp({
-      "id": "com.chaoxing.mobile",
-      "name": "学习通",
-      "groups": [
+  "id": "com.chaoxing.mobile",
+  "name": "学习通",
+  "groups": [
+    {
+      "key": 1,
+      "name": "功能类-学习通电脑端自动登录",
+      "desc": "学习通电脑端自动登录",
+      "rules": [
         {
-          "key": 1,
-          "name": "功能类-学习通电脑端自动登录",
-          "desc": "学习通电脑端自动登录",
-          "rules": [
-            {
-              "action": "clickCenter",
-              "matches": [
-                "@[desc=\"登录\"][visibleToUser=true] < View <2 [text=\"登录确认\"]"
-              ],
-              "activityIds": [
-                "com.chaoxing.mobile.webapp.ui.WebAppViewerActivity"
-              ]
-            }
+          "action": "clickCenter",
+          "matches": [
+            "@[desc=\"登录\"][visibleToUser=true] < View <2 [text=\"登录确认\"]"
+          ],
+          "activityIds": [
+            "com.chaoxing.mobile.webapp.ui.WebAppViewerActivity"
           ]
         }
       ]
+    }
+  ]
 });
